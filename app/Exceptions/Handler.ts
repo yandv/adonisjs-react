850c9a7d225827827425c41d78b5465dc6cae42e
yandv <allangamerbr252@gmail.com>
2023-04-05 23:34:29 -0300

    First commit

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -13,15 +13,11 @@
 |
 */
 
-import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
-  protected statusPages = {
-    '403': 'errors/unauthorized',
-    '404': 'errors/not-found',
-    '500..599': 'errors/server-error',
-  }
+  protected statusPages = {}
 
   constructor() {
     super(Logger)
